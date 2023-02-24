3b0176c24f383b4fd32ebfcbbe675abda2215ba4
Priyanshu Rai <priyanshu.rai891@gmail.com>
2023-02-25 01:46:59 +0530

    Added Prettier & config for code formatting

---
+++ UI/prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+   // removes trailing commas
+   trailingComma: 'none',
+   // ensures tabs are 2 spaces
+   tabWidth: 3,
+   // removes semicolons
+   semi: false,
+   // 'this' not "this"
+   singleQuote: true,
+   // disable { adding: 'this spacing' }
+   bracketSpacing: false,
+   bracketSameLine: true,
+   printWidth: 100
+}
