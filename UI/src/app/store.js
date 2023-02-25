712e26deaaf970a67b5fc1dd818542312f6da5fd
Priyanshu Rai <priyanshu.rai891@gmail.com>
2023-02-25 14:26:26 +0530

    Added redux toolkit setup

---
+++ UI/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import dataReducer from '../app/features/dataSlice'
+
+export const store = configureStore({
+  reducer: {
+    data: dataReducer
+  },
+})
