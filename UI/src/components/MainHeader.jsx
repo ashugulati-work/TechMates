b5dde0e9ccdb0b1b7d3bf4ced07a6f69f1ac813f
Priyanshu Rai <priyanshu.rai891@gmail.com>
2023-02-25 10:41:07 +0530

    Added logo

--- UI/src/components/MainHeader.jsx
+++ UI/src/components/MainHeader.jsx
@@ -4,14 +4,16 @@ import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import AdbIcon from '@mui/icons-material/Adb'
+import Icons from "../assets/logo.svg"; 
 
 const MainHeader = () => {
    return (
       <Box sx={{flexGrow: 1}}>
          <AppBar position="static">
             <Toolbar>
-               <AdbIcon sx={{fontSize: '3rem', display: {xs: 'none', md: 'flex'}, mr: 1}} />
-               <Typography variant="h4" component="div" sx={{flexGrow: 1}}>
+               {/* <AdbIcon sx={{fontSize: '3rem', display: {xs: 'none', md: 'flex'}, mr: 1}} /> */}
+               <img src={Icons} alt="" height="30px" width="30px" style={{marginRight:'1rem', color:'darkblue'}}/>
+               <Typography variant="h4" component="div" sx={{flexGrow: 1, fontWeight:'600'}}>
                   SynthAI
                </Typography>
             </Toolbar>
